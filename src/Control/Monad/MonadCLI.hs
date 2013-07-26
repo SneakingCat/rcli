dfd44710e4b56f62465578dd694ee76391043c83
Patrik Sandahl <sneakingcatsw@gmail.com>
2013-07-26 23:12:44 +0200

    Added the MonadCLI module.

--- src/Control/Monad/MonadCLI.hs
+++ src/Control/Monad/MonadCLI.hs
@@ -1,3 +1,17 @@
-module Control.Monad.MonadCLI where
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Control.Monad.MonadCLI (
+  CLI
+  , runCLI
+  ) where
 
-import Control.Monad.State
+import Control.Monad.State (MonadState, MonadIO, StateT, evalStateT)
+
+-- | The monadic CLI type. Is wrapping StateT with the IO monad at the
+-- bottom
+newtype CLI s a = CLI {
+  extractCLI :: StateT s IO a
+  } deriving (Monad, MonadState s, MonadIO)
+             
+-- | Run the CLI monad, leaving only the IO a part left
+runCLI :: CLI s a -> s -> IO a
+runCLI m s = evalStateT (extractCLI m) s
