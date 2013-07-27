b671da6ca475757da92dca8270a89edc2e38c677
Patrik Sandahl <sneakingcatsw@gmail.com>
2013-07-27 23:51:03 +0200

    Added more qc test generator functionality.

--- tests/LineParserTest.hs
+++ tests/LineParserTest.hs
@@ -2,9 +2,19 @@ module LineParserTest where
 
 import System.Console.LineParser (CommandLine (..), fromString, toString)
 import Test.QuickCheck
+import Control.Applicative ((<$>), (<*>))
 
 instance Arbitrary CommandLine where
-  arbitrary = return (CommandLine "Sigge")
+  arbitrary = do
+    cmd <- identifier    
+    return (CommandLine cmd)
+
+-- | Generate an identifier string
+identifier :: Gen String
+identifier = (:) <$> letter <*> listOf otherIdChars
+  where
+    letter = oneof [choose ('a', 'z'), choose ('A', 'Z')]
+    otherIdChars = elements "0123456789_-"
 
 -- | The property is given a command line in the internal format. When
 -- converted to and then from the string format the result shall be
