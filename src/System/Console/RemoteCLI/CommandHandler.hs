54af11337b1fd56812046bd14fe77ed4b8ce6123
Patrik Sandahl <sneakingcatsw@gmail.com>
2013-08-01 21:01:57 +0200

    Refactoring, replaced string concatenation with printf.

--- src/System/Console/RemoteCLI/CommandHandler.hs
+++ src/System/Console/RemoteCLI/CommandHandler.hs
@@ -2,13 +2,14 @@ module System.Console.RemoteCLI.CommandHandler where
 
 import System.Console.RemoteCLI.CommandLine
 import System.Console.RemoteCLI.CommandState
+import Text.Printf
 import qualified Data.Map.Strict as M
 
 -- | Lookup the pure handler for the given command line
 lookupHandler :: CommandLine -> CommandState -> Either String PureCommandHandler
 lookupHandler cl@(CommandLine scope cmd opt) state =
   case M.lookup cmd (select scope) of
-    Nothing       -> Left $ "Command " ++ (show cmd) ++ " not found"
+    Nothing       -> Left $ printf "Command \"%s\" not found" cmd
     Just handler  -> Right handler
     where
       select Local = localCommands state
