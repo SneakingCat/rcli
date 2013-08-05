a3e67e7ce8acb07613de820618072043dc24f36a
Patrik Sandahl <sneakingcatsw@gmail.com>
2013-08-05 14:29:14 +0200

    Integrated the local commands into the initial state.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
 import Control.Monad.MonadCLI (runCLI)
-import System.Console.RemoteCLI.CommandState (empty)
+import System.Console.RemoteCLI.CommandHandler (localHandlers)
+import System.Console.RemoteCLI.CommandState (CommandState (..)
+                                              , empty
+                                              , fromList)
 import System.Console.RemoteCLI.Executor (evalLoop)
 
 main :: IO ()
-main = runCLI evalLoop empty
+main = runCLI evalLoop initialState
+
+initialState :: CommandState
+initialState = let (CommandState v _ r _) = empty
+                   l'                     = fromList localHandlers
+               in (CommandState v l' r l')
